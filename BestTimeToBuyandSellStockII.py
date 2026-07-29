93d6bd205109b1244346b1fdcda111fccff0050c
deba.ai <dp0000000004@gamil.com>
2026-07-29 08:27:23 +0530

    +

--- BestTimeToBuyandSellStockII.py
+++ BestTimeToBuyandSellStockII.py
@@ -4,12 +4,28 @@ class Solution:
 
         min_number = float('inf')
         max_profit = 0
+        buy = 0
+        sell = 0
 
+        for i in range(len(prices)):
+            if prices[i] < min_number:
+                min_number = prices[i]
+
+            elif prices[i] > min_number:
+                buy = min_number
+                for i in range(i, len(prices)):
+                    if prices[i] > buy:
+                        continue
+                    elif prices[i] < buy:
+                        sell = prices[i-1]
+                        max_profit += sell - buy
+
+            return max_profit
         
 
 
 
-nums = [2,9]
+nums = [2,9, 1]
 sol = Solution()
 ans = sol.bestTimeToBuyAndSellII(nums)
 print(ans)
